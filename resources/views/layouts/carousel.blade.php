636fd4c0b3bff38f0f01389410835d5eab9c8067
Quentin Vilcoque <q.vilcoque@gmail.com>
2023-12-18 18:45:16 +0100

    Update carousel.blade.php

--- resources/views/layouts/carousel.blade.php
+++ resources/views/layouts/carousel.blade.php
@@ -1,30 +1,19 @@
 <!-----------carousel----------------->
-<section class="hero">
-    <h1>Nouveautés</h1>
 
-    <h2>Nouvelles Séries</h2>
-    <div id="carouselNewSeries" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            @foreach($dataSeries->take(8) as $index => $serie)
-                <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
-                    <img src="https://image.tmdb.org/t/p/w500/{{ $serie['poster_path'] }}" class="d-block w-100" alt="{{ $serie['name'] }}">
-                    <div class="carousel-caption d-none d-md-block">
-                        <p>{{ $serie['name'] }}</p>
+<section class="hero">
+    <div class="hero__slider owl-carousel">
+        @foreach ($dataSeries->take(8) as $index => $serie)
+            <div class="hero__items set-bg" data-setbg="https://image.tmdb.org/t/p/w500/{{ $serie['poster_path'] }}">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="hero__text">
+                            <h2>{{ $serie['name'] }}</h2>
+                            <a href="#"><span>Watch Now</span> <i class="fa fa-angle-right"></i></a>
+                        </div>
                     </div>
                 </div>
-            @endforeach
-        </div>
-        <a class="carousel-control-prev" href="#carouselNewSeries" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselNewSeries" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+            </div>
+        @endforeach
     </div>
-
-
-
 </section>
 <!---------------fin carousel------------------------>
