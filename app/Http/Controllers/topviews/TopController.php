1f93c475e8587fe7ed19b92ecff922ae39e33fd5
Quentin Vilcoque <q.vilcoque@gmail.com>
2023-12-19 08:37:09 +0100

    les tops

---
+++ app/Http/Controllers/topviews/TopController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Top;
+
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+
+class TopController extends Controller
+{
+    /**
+     * Page d'acceuil du site
+     *
+     * @use /
+     *
+     * @return Application|Factory|\Illuminate\Contracts\View\View
+     * @throws Exception
+     */
+
+    public function show(Request $request)
+    {
+
+        $token = env('TMDB_API_KEY');
+        $topSeries = 'https://api.themoviedb.org/3/tv/popular?language=fr-FR&page=1'; // Endpoint pour les séries populaires
+        $topMovies = 'https://api.themoviedb.org/3/movie/popular?language=fr-FR&page=1'; // Endpoint pour les films populaires
+
+        // Series.
+
+        $client = new Client();{
+            $responseSeries = $client->get($topSeries, [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $token,
+                    'Accept' => 'application/json',
+                ],
+            ]);
+
+            // Films.
+
+            $responseMovies = $client->get($topMovies, [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $token,
+                    'Accept' => 'application/json',
+                ],
+            ]);
+
+            // Maintenant, on stocke les données des séries et des films.
+
+            $dataSeries = Collection::make(json_decode($responseSeries->getBody(), true)['results']);
+            $dataMovies = Collection::make(json_decode($responseMovies->getBody(), true)['results']);
+
+            view('layouts.topviws', ['newSeries' => $dataSeries, 'newMovies' => $dataMovies]);
+
+        }
+
+    }
+
+}
--- resources/views/layouts/topviews.blade.php
+++ resources/views/layouts/topviews.blade.php
@@ -1,6 +1,6 @@
-<div class="col-lg-4 col-md-6 col-sm-8" >
+<div class="col-lg-4 col-md-6 col-sm-8">
     <div class="product__sidebar" style="background-color: #171717">
-        <div class="product__sidebar__view" >
+        <div class="product__sidebar__view">
             <div class="section-title">
                 <h5>Top Views</h5>
             </div>
@@ -10,38 +10,19 @@
                 <li data-filter=".month">Month</li>
                 <li data-filter=".years">Years</li>
             </ul>
-            <div class="filter__gallery">
-                <div class="product__sidebar__view__item set-bg mix day years"
-                    data-setbg="img/sidebar/tv-1.jpg">
-                    <div class="ep">18 / ?</div>
-                    <div class="view"><i class="fa fa-eye"></i> 9141</div>
-                    <h5><a href="#">Boruto: Naruto next generations</a></h5>
-                </div>
-                <div class="product__sidebar__view__item set-bg mix month week"
-                    data-setbg="img/sidebar/tv-2.jpg">
-                    <div class="ep">18 / ?</div>
-                    <div class="view"><i class="fa fa-eye"></i> 9141</div>
-                    <h5><a href="#">The Seven Deadly Sins: Wrath of the Gods</a></h5>
-                </div>
-                <div class="product__sidebar__view__item set-bg mix week years"
-                    data-setbg="img/sidebar/tv-3.jpg">
-                    <div class="ep">18 / ?</div>
-                    <div class="view"><i class="fa fa-eye"></i> 9141</div>
-                    <h5><a href="#">Sword art online alicization war of underworld</a></h5>
-                </div>
-                <div class="product__sidebar__view__item set-bg mix years month"
-                    data-setbg="img/sidebar/tv-4.jpg">
-                    <div class="ep">18 / ?</div>
-                    <div class="view"><i class="fa fa-eye"></i> 9141</div>
-                    <h5><a href="#">Fate/stay night: Heaven's Feel I. presage flower</a></h5>
-                </div>
-                <div class="product__sidebar__view__item set-bg mix day" data-setbg="img/sidebar/tv-5.jpg">
-                    <div class="ep">18 / ?</div>
-                    <div class="view"><i class="fa fa-eye"></i> 9141</div>
-                    <h5><a href="#">Fate stay night unlimited blade works</a></h5>
-                </div>
-            </div>
-        </div>
+            @foreach ($dataMovies->take(4) as $movie)
+                <div class="filter__gallery">
+                    <div class="product__sidebar__view__item set-bg mix day years" data-setbg="https://image.tmdb.org/t/p/w500/{{ $movie['poster_path'] }}">
 
+                        <h5><a href="#">{{ $movie['original_title'] }}</a></h5>
+                    </div>
+            @endforeach
+
+
+
+
+        </div>
     </div>
+
+</div>
 </div>
