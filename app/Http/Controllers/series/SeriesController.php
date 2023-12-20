e1c4703c23061533b1699f08914ac0ef6b478ac4
Quentin Vilcoque <q.vilcoque@gmail.com>
2023-12-20 02:44:05 +0100

    page series

--- app/Http/Controllers/series/SeriesController.php
+++ app/Http/Controllers/series/SeriesController.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace App\Http\Controllers\Series;
+
 use App\Http\Controllers\Controller;
-
-
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 class SeriesController extends Controller
 {
-        /**
+    /**
      * Page d'acceuil du site
      *
      * @use /
@@ -17,10 +18,63 @@ class SeriesController extends Controller
      * @throws Exception
      */
 
-     public function show(Request $request){
+    public function show(Request $request)
+    {
+        // Récupérer le numéro de la page actuelle à partir de la requête
+        $page = $request->input('page', 1);
 
-        return view('series.series');
+        // Vérifier que la page est dans la plage valide (1 à 500)
+        $page = max(1, min(500, $page));
 
-     }
+        $token = env('TMDB_API_KEY');
+        $urlSeries = "https://api.themoviedb.org/3/tv/popular?language=fr-FR&page=$page";
+
+        // Series.
+
+        $client = new Client();{
+            $responseSeries = $client->get($urlSeries, [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $token,
+                    'Accept' => 'application/json',
+                ],
+            ]);
+
+            // Maintenant, on stocke les données des séries.
+
+            $dataSeries = Collection::make(json_decode($responseSeries->getBody(), true)['results']);
+            $totalPages = json_decode($responseSeries->getBody(), true)['total_pages'];
+
+
+
+            $content = [$dataSeries];
+
+            // Triez le contenu par date de sortie décroissante
+            // Triez le contenu par date de sortie décroissante
+            usort($content, function ($a, $b) {
+                $dateA = $a['release_date'] ?? $a['first_air_date'] ?? null;
+                $dateB = $b['release_date'] ?? $b['first_air_date'] ?? null;
+
+                if ($dateA === null && $dateB === null) {
+                    return 0;
+                }
+
+                if ($dateA === null) {
+                    return 1;
+                }
+
+                if ($dateB === null) {
+                    return -1;
+                }
+
+                return strtotime($dateB) - strtotime($dateA);
+            });
+
+            // on retourne les données
+
+            return view('series.series', ['series' => $dataSeries,'page' => $page,'totalPages' => $totalPages]);
+
+        }
+
+    }
 
 }
--- resources/views/series/series.blade.php
+++ resources/views/series/series.blade.php
@@ -5,6 +5,44 @@
 ])
 
 @section('content')
-SERIES
+    <section class="product spad">
+        <div class="row">
+            <div class="col-lg-24">
+                <div class="trending__product" style="background-color: #171717">
+                    <!-----------------------SERIES ----------->
+                    <div class="row">
+                        <div class="col-lg-8 col-md-8 col-sm-8">
+                            <div class="section-title">
+                                <h4>SERIES</h4>
+                            </div>
+                        </div>
 
+                    </div>
+                    <div class="row">
+                        @foreach ($series as $serie)
+                            <div class="col-md-3">
+                                <img src="https://image.tmdb.org/t/p/w500/{{ $serie['poster_path'] }}"
+                                    alt="{{ $serie['name'] }}">
+                                <p style="color: white">{{ $serie['name'] }}</p>
+                            </div>
+                        @endforeach
+                    </div>
+                    <div class="d-flex justify-content-center" style="gap: 10px;">
+                        {{-- Boutons suivants et précédents --}}
+                        @if ($page > 1)
+                            <a href="{{ route('series.series.show', ['page' => $page - 1]) }}"
+                                class="btn btn-primary">Précédent</a>
+                        @endif
+
+
+                        @if (isset($totalPages) && $page < $totalPages)
+                            <a href="{{ route('series.series.show', ['page' => $page + 1]) }}"
+                                class="btn btn-primary">Suivant</a>
+                        @endif
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
 @endsection
