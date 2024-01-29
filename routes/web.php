73a42e7ed04c63b492bf6d3757f9695d40ddd4cc
Quentin Vilcoque <q.vilcoque@gmail.com>
2024-01-29 22:00:00 +0100

    Merge branch 'master' of https://github.com/Qv-code-Qv/ListooMovie

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\movies\MoviesController;
+use App\Http\Controllers\series\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,8 +14,38 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
+
+/*Home*/
+
+Route::group([], function () {
+
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+
 });
+
+/*Movies*/
+Route::group(['prefix' => 'movies'], function () {
+
+    Route::get('/', [MoviesController::class, 'show'])->name('movies.movies');
+    Route::get('/{id}', [MoviesController::class, 'showDetails'])->name('movies.details_movies');
+
+
+
+});
+
+
+/*Series*/
+Route::group(['prefix' => 'series'], function () {
+
+    Route::get('/', [SeriesController::class, 'show'])->name('series.series');
+    Route::get('/{id}', [SeriesController::class, 'showDetails'])->name('series.details_series');
+
+
+});
+
+
+
+
+
