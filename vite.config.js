3ea26b44e58b6ed503b26281646202b5de00f2fe
Quentin Vilcoque <q.vilcoque@gmail.com>
2025-01-23 14:39:57 +0100

    update deploi
    fix bug

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import laravel from 'vite-plugin-laravel';  // ou un autre plugin approprié
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
